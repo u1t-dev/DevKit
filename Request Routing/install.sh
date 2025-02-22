69ac7da169ce3de49716f567180036d98d2f3b7c
u1terior <119457323+u1t-dev@users.noreply.github.com>
2025-02-22 18:35:58 -0500

    Create install.sh

---
+++ Request Routing/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DATA=$(curl -X GET https://raw.githubusercontent.com/u1t-dev/DevKit/refs/heads/main/Request%20Routing/index.js)
+
+touch route.js
+echo "$DATA" > route.js
+echo "Made route.js file!"
+chmod +x route.js
+echo "Made route.js executable!"
