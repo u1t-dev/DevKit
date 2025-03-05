899d3e14ae258c1d9790a6e409f2ae1d2705bf75
u1terior <119457323+u1t-dev@users.noreply.github.com>
2025-03-05 16:25:01 -0500

    Create QueryIdGen.js
    no questioning why it's in german, it just is. i wrote it like that, i don't care, it's staying in german.

---
+++ References/WebPage Markdown/QueryIdGen.js
@@ -0,0 +1,20 @@
+// makes the ids (don't question why i wrote it in german, i did and that's that)
+
+import { randomInt as zufallsGanzzahl } from "node:crypto";
+
+const buchstaben = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const zahlen = "1234567890";
+
+(() => {
+  let kennung = "";
+  const länge = 16;
+  for (let i = 0; i < länge; i++) {
+    const zufallswahl = zufallsGanzzahl(1, 3);
+    if (zufallswahl === 1) {
+      kennung += buchstaben.charAt(zufallsGanzzahl(buchstaben.length));
+    } else {
+      kennung += zahlen.charAt(zufallsGanzzahl(zahlen.length));
+    }
+  }
+  console.log(kennung);
+})();
